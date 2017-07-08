f0ff053fdaedb6e1bd8d604cf9d3912b75e2b51a
于可汗 <njyukehan@gmail.com>
2017-07-08 01:33:49 -0500

    Add files via upload

--- Plugin.php
+++ Plugin.php
@@ -52,8 +52,8 @@ class LaTeXTypecho_Plugin implements Typecho_Plugin_Interface
         extensions: ["tex2jax.js"],
         jax: ["input/TeX", "output/HTML-CSS"],
         tex2jax: {
-            inlineMath: [\'$\',\'$\'],
-            displayMath: [\'$$\',\'$$\'],
+            inlineMath: [[\'$\',\'$\']],
+            displayMath: [[\'$$\',\'$$\']],
             processEscapes: true
         },
         "HTML-CSS": { availableFonts: ["TeX"] }
